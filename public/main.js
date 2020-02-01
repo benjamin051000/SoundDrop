127620f183978d1cbfc20ea258064030e1b810f1
John Shoemaker <jshoemaker9999@gmail.com>
2020-02-01 14:04:57 -0500

    smaller send time

--- public/main.js
+++ public/main.js
@@ -9,34 +9,34 @@ function playSound() {
   console.log(values);
 
   // TODO Convert numbers into cooresponding frequencies using dict
-  let freqs = {
-    '0': 440,
-    '1': 540,
-    '2': 640,
-    '3': 740,
-    '4': 840,
-    '5': 940,
-    '6': 1040,
-    '7': 1140,
-    '8': 1240,
-    '9': 1340,
-    'A': 1440,
-    'B': 1540,
-    'C': 1640,
-    'D': 1740,
-    'E': 1840,
-    'F': 1940
+  let freqs = { //5000 - 5050 works
+    '0': 5000,
+    '1': 5050,
+    '2': 5100,
+    '3': 5150,
+    '4': 5200,
+    '5': 5250,
+    '6': 5300,
+    '7': 5350,
+    '8': 5400,
+    '9': 5450,
+    'A': 5500,
+    'B': 5550,
+    'C': 5600,
+    'D': 5650,
+    'E': 5700,
+    'F': 5750
   }; //etc. TODO Use a function to do this
 
   // Play each sound
-  const playTime = 1;
+  const playTime = 1/8;
   for (let j = 0; j < values.length; j++)
   {   
     osc = new p5.Oscillator();
     osc.setType('sine');
     osc.freq(freqs[values[j]]);
     osc.amp(1);
-    osc.start(j);
-    osc.stop(j+playTime);
+    osc.start(j*playTime);
+    osc.stop(j*playTime+playTime/2);
   }
 }
