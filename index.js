c2fea40742097e93e5f9f4dd6b517fa5bd02f331
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2020-02-01 18:22:47 -0500

    Changed name of main HTML

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const port = 3000
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
