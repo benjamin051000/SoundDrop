bff04e2f16d18dbf401e5ce56bb9a29076907832
joshuahaddad <joshuahaddad@ufl.edu>
2020-02-01 12:59:21 -0500

    Binary listening

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ function setup(){
   noFill();
   fft = new p5.FFT();
   let myDiv = createDiv('click to start audio');
-  myDiv.position(0, 0);
+    myDiv.position(0, 0);
 
   let mySynth = new p5.MonoSynth();
 
@@ -17,16 +17,69 @@ function setup(){
      myDiv.remove();
    });
 }
+var zero_counter = 0
+var one_counter = 0
+let current_frequency = 440
+let counter = 0
+let offset_frequency = 0
 function draw(){
   //If mic has been activated, run this shit
   if(mic){
     //Set the input of the fft to the microphone input
     fft.setInput(mic)
-    print(fft)
+    //print(fft)
     background(200);
 
     //Get the fft data into a useable object
     let spectrum = fft.analyze();
+
+    //console.log(fft.getEnergy(440) > 150)
+    var frequencies = [440, 540]
+    var value = {440: 0, 540: 1}
+    for(var v = 0; v < frequencies.length; v++){
+      if(fft.getEnergy(frequencies[v]) > 150){
+          //console.log(`Frequency ${frequencies[v]} energy ${fft.getEnergy(frequencies[v])}`)
+          if(frequencies[v] != current_frequency){
+            offset_frequency += 1;
+            current_frequency = frequencies[v];
+            if(offset_frequency > 40)
+            {
+              console.log("Offset triggered")
+              counter = 0;
+              offset_frequency = 0;
+              //current_frequency = frequencies[v]
+            }
+          }
+
+          counter += 1
+          //console.log(counter)
+          if(counter >= 55){
+            console.log(value[frequencies[v]]);
+            counter = 0
+          }
+
+    }
+    }
+    // if(fft.getEnergy(540) > 150){
+    //   one_counter += 1
+    // }
+    // if(fft.getEnergy(440) > 150){
+    //   zero_counter += 1
+    // }
+    // if(fft.getEnergy(440) < 150){
+    //   zero_counter = 0
+    // }
+    // if(fft.getEnergy(540) < 150){
+    //   one_counter = 0
+    // }
+    // if(one_counter > 50){
+    //   console.log("1")
+    //   one_counter = 0
+    // }
+    // if(zero_counter > 50){
+    //   console.log("0")
+    //   zero_counter = 0
+    // }
 
     //Draw the fft
     beginShape();
@@ -35,10 +88,11 @@ function draw(){
     }
     endShape();
 
+
     //Information from the mic
     micLevel = mic.getLevel();
-    print(micLevel)
-    print(mic)
+    //print(micLevel)
+    //print(mic)
     ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
 }}
 
@@ -49,7 +103,7 @@ function takeInput()
     let input = document.getElementById("inputBox").value;
     let parsedInput = parseInt(input)
     //Make signal based off input
-    alert(parsedInput)
+    //alert(parsedInput)
     mic = new p5.AudioIn()
     mic.start()
 
