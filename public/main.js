7c533aa46616967dec24e7fdea10aa260e217340
joshuahaddad <joshuahaddad@ufl.edu>
2020-02-01 00:19:01 -0500

    bruh

--- public/main.js
+++ public/main.js
@@ -1,14 +1,21 @@
-function setup() {
-    console.log("setting up p5...")
-}
-function draw() {
 
+let mic;
+function setup(){
+  mic = new p5.AudioIn()
+  mic.start();
 }
+function draw(){
+  background(0);
+  micLevel = mic.getLevel();
+  ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
+}
+
 function takeInput()
 {
     //Get input
     let input = document.getElementById("inputBox").value;
     let parsedInput = parseInt(input)
+
 
     //declare oscillator
     // osc = new p5.Oscillator();
