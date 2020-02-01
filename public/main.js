30200996ed4e246c1c42d3721b5b65fb953bc228
joshuahaddad <joshuahaddad@ufl.edu>
2020-02-01 14:27:23 -0500

    ben is a weeb

--- public/main.js
+++ public/main.js
@@ -35,35 +35,32 @@ function draw(){
     let spectrum = fft.analyze();
 
     //console.log(fft.getEnergy(440) > 150)
-    var frequencies = [5000, 5050, 5100, 5150, 5200, 5250, 5300, 5350, 5400, 5450, 5500, 5550, 5600, 5650, 5700, 5750]
-    var value = { //5000 - 5050 works
-    '0': 5000,
-    '1': 5050,
-    '2': 5100,
-    '3': 5150,
-    '4': 5200,
-    '5': 5250,
-    '6': 5300,
-    '7': 5350,
-    '8': 5400,
-    '9': 5450,
-    'A': 5500,
-    'B': 5550,
-    'C': 5600,
-    'D': 5650,
-    'E': 5700,
-    'F': 5750
+    var frequencies = [5000, 5050, 5100, 5150, 5200, 5250, 5300, 5350, 5400, 5450, 5500, 5550, 5600, 5650, 5700, 5750];
+    var value =  { //5000 - 5050 works
+    5000: '0',
+    5050: '1',
+    5100: '2',
+    5150: '3',
+    5200: '4',
+    5250: '5',
+    5300: '6',
+    5350: '7',
+    5400: '8',
+    5450: '9',
+    5500: 'A',
+    5550: 'A',
+    5600: 'C',
+    5650: 'D',
+    5700: 'E',
+    5750: 'F'
   };
     //print(fft.getEnergy(5000) > 140)
     for(var v = 0; v < frequencies.length; v++){
-      if(fft.getEnergy(frequencies[v]) > 140){
+      if(fft.getEnergy(frequencies[v]) > 110){
           //console.log(`Frequency ${frequencies[v]} energy ${fft.getEnergy(frequencies[v])}`)
           //console.log(frequencies[v])
-          if(frequencies[v] == 5000){
-            //print(0)
-          }
-          else{//print(1)
-          }
+          //print(value[frequencies[v]])
+
           if(frequencies[v] != current_frequency){
             offset_frequency += 1;
             current_frequency = frequencies[v];
@@ -80,7 +77,7 @@ function draw(){
 
           counter += 1
           //console.log(counter)
-          if(counter >= 8){
+          if(counter >= 7){
             bitstr += value[frequencies[v]]
             console.log(bitstr);
             counter = 0
