02ea4fbda59c6a0daefb5e56ae041118e0b1100b
John Shoemaker <jshoemaker9999@gmail.com>
2020-02-01 00:12:57 -0500

    pushed

--- public/main.js
+++ public/main.js
@@ -10,6 +10,17 @@ function takeInput()
     let input = document.getElementById("inputBox").value;
     let parsedInput = parseInt(input)
 
+    //declare oscillator
+    // osc = new p5.Oscillator();
+    // osc.setType('sine');
+    // osc.freq(440);
+    // osc.amp(1);
+
     //Make signal based off input
     alert(parsedInput)
+
+    if(parsedInput == 1)
+    {
+        //osc.start();
+    }
 }
